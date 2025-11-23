4ebfdaa055c5217b83ccacb080e31b7f03f714ab
luismallebrera <38189359+luismallebrera@users.noreply.github.com>
2025-11-23 15:02:06 +0000

    Remove main-navigation from layout-2, show only toggle and dropdown menu

--- template-parts/layout-2.php
+++ template-parts/layout-2.php
@@ -31,17 +31,6 @@
 			</div>
 		</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-
 		<nav class="site-navigation-dropdown" aria-label="Mobile menu" aria-hidden="true">
 			<div class="site-navigation-background"></div>
 			<?php
